f1cf55ed43f5e0eb4a71ff3beae4cd7e50ee9f64
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-11-29 18:11:22 -0500

    Delete db_config_function.php
    NOT SAFE HAVING THIS HERE.

--- db_config_function.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$f_server = "imc.kean.edu";
-	$f_login = "seds2017";
-	$f_password = "2017fall";
-	$f_database = "seds2017db";
-
-	$f_conex = mysqli_connect($f_server, $f_login, $f_password, $f_database) or die("Error: Unable to connect to DataBase.");	
-?>
