89a31f2a0ef178514e557d33c19bf2ca42723627
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-11-29 18:11:15 -0500

    Delete db_config.php
    NOT SAFE HAVING THIS HERE.

--- db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$server = "imc.kean.edu";
-	$login = "seds2017";
-	$password = "2017fall";
-	$database = "seds2017db";
-
-	$conex = mysqli_connect($server, $login, $password, $database) or die("Error: Unable to connect to DataBase.");	
-?>
