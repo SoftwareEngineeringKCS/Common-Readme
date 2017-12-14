dfcf14f49fc8f218e7b1934bca1f30da6c61e21c
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-12-14 01:07:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+	require_once ('includes/Mobile_Detect.php');
+	$detect = new Mobile_Detect;
+	if ($detect->isMobile()) {
+		header("Location: mobile/index.php");	
+	}
+
 	$page_title = "Kean Career Services";
 	include ('includes/header.html');
 
